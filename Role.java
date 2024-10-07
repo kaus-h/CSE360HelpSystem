f2b06094953ee23bc65997047472958df5a5a7d7
oshposhbegoshkaush <kaustav.kalra123@gmail.com>
2024-10-07 15:46:55 -0700

    Update Role.java

--- Role.java
+++ Role.java
@@ -1,1 +1,31 @@
+package com.usermanagement;
 
+public class Role {
+    public static final String ADMIN = "Admin";
+    public static final String STUDENT = "Student";
+    public static final String INSTRUCTOR = "Instructor";
+
+    private String roleName;
+
+    // Constructors
+    public Role() {}
+
+    public Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    // Getter and setter
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    // Override toString() for better readability
+    @Override
+    public String toString() {
+        return "Role{" + "roleName='" + roleName + '\'' + '}';
+    }
+}
