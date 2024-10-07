b430e8324f361c3b4111ff7dfa6f4e6f9023d603
oshposhbegoshkaush <kaustav.kalra123@gmail.com>
2024-10-07 15:48:39 -0700

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,1 +1,37 @@
+package com.usermanagement;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Database {
+    private Map<String, User> userTable;
+
+    public Database() {
+        this.userTable = new HashMap<>();
+    }
+
+    // Method to add a new user to the database
+    public void addUser(User user) {
+        userTable.put(user.getUsername(), user);
+    }
+
+    // Method to delete a user from the database by username
+    public void deleteUser(String username) {
+        userTable.remove(username);
+    }
+
+    // Method to get a user from the database by username
+    public User getUser(String username) {
+        return userTable.get(username);
+    }
+
+    // Method to update a user in the database
+    public void updateUser(User user) {
+        userTable.put(user.getUsername(), user);
+    }
+
+    // Method to list all users in the database
+    public Map<String, User> listUsers() {
+        return new HashMap<>(userTable);
+    }
+}
